2d6ef8c0694f7e1a299f901cadc12f6f79444902
SAC <sachin@crftech360.com>
2025-01-15 17:29:08 +0530

    fixed bat file

--- start.bat
+++ start.bat
@@ -1,6 +1,4 @@
 @echo off
-cd /d "%~dp0"               :: Switch to the directory containing the batch file
-cd env\Scripts              :: Navigate to the virtual environment scripts folder
-call activate               :: Activate the virtual environment
-cd ../..                    :: Navigate back to the project root
-python app.py               :: Run the Python application
+call env\Scripts\activate
+python app.py
+pause
